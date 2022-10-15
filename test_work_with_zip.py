c0a70c347fc5bee904d0d994aa3c4c070d8f65ca
dsmnv <dmitry.d.sem@gmail.com>
2022-10-15 14:17:54 +0300

    test_work_with_zip.py added

--- test_work_with_zip.py
+++ test_work_with_zip.py
@@ -1,4 +1,22 @@
-from zipfile import ZipFile
+import zipfile
+import os
+import shutil
 
-zip = ZipFile('resources.zip')
-print(zip.namelist())
+
+def test_create_zip():
+    newzip = zipfile.ZipFile(os.path.abspath('file.zip'), "w")
+    newzip.write('resources/CSV.csv')
+    newzip.write('resources/JPEG.jpeg')
+    newzip.write('resources/PDF.pdf')
+    newzip.write('resources/XLS.xls')
+    newzip.write('resources/XLSX.xlsx')
+
+
+def test_move_zip_in_new_folder():
+    file_path = "file.zip"
+    destination_path = "resources"
+    shutil.move(file_path, destination_path)
+
+
+def test_remove_zip():
+    os.remove('resources/file.zip')
